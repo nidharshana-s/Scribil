d0a0eec15c0b7dba07d8a5c0cc0185f533b2b782
Nidharshana S <nidharshana0402@gmail.com>
2025-01-25 07:34:06 +0530

    search bar done

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Navbar from '../../components/NavBar/navbar'
+import NoteCard from '../../components/Cards/NoteCard'
 const home = () => {
   return (
-    <div>home</div>
+    <>
+    <Navbar />
+    
+    </>
   )
 }
 
