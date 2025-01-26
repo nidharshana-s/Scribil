32734e7d43eb50efc64abeb949f7bd907d99caff
Nidharshana S <nidharshana0402@gmail.com>
2025-01-26 12:22:09 +0530

    scribil

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -23,7 +23,7 @@ const navbar = ({userInfo}) => {
   return (
     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
         <h2 className='text-xl font-medium text-black py-2'>
-            Notes
+            Scribil
         </h2>
 
         <SearchBar 
