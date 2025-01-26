eb2f2cd9ccae3960f58b38bc6faaf32de6a1efc8
Nidharshana S <nidharshana0402@gmail.com>
2025-01-26 17:18:05 +0530

    doneeeee

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 //const tailwindcss = require("@tailwindcss/vite")
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react(), tailwindcss()],
   build:{
     outDir:'dist'
   }
